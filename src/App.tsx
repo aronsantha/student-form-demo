1dd96aab3250407e47a7c1fd5278bf0ae3c74ac2
aronsantha <108655791+aronsantha@users.noreply.github.com>
2025-11-11 17:34:53 +0100

    display enrolled students or empty state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useState } from "react";
+
 // import { courses, subjects } from "./constants";
 
 function App() {
+  const [enrolledStudents, setEnrolledStudents] = useState([]);
+
   return (
     <>
       <div className="mx-auto flex h-screen items-center justify-center">
@@ -11,7 +15,7 @@ function App() {
             <div>
               <div className="mb-5">
                 <h2 className="text-xl font-bold">Register</h2>
-                <p className="text-sm text-neutral-600">
+                <p className="text-xs text-neutral-600">
                   Fill in and submit the form to register yourself
                 </p>
               </div>
@@ -20,12 +24,21 @@ function App() {
             <div>
               <div>
                 <div className="mb-5">
-                  <h2 className="text-xl font-bold">Enrollled students</h2>
-                  <p className="text-sm text-neutral-600">
+                  <h2 className="text-xl font-bold">Enrolled students</h2>
+                  <p className="text-xs text-neutral-600">
                     See and edit enrolled students.
                   </p>
                 </div>
-                <div>LIST</div>
+                {enrolledStudents.length === 0 ? (
+                  <div className="rounded bg-neutral-100 py-4">
+                    <p className="text-center text-sm text-neutral-600">
+                      No students have enrolled yet. Click the "Register" button
+                      to enroll.
+                    </p>
+                  </div>
+                ) : (
+                  <div>{enrolledStudents}</div>
+                )}
               </div>
             </div>
           </div>
